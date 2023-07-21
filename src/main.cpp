cc5be0b55d9aa08ac35a1a98b94e2d32eca73a45
Marc Owen <marcowen20@gmail.com>
2023-07-21 13:08:29 +0900

    Added argparse

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,34 @@
+#include "mu-emu/rv32i.h"
 #define FMT_HEADER_ONLY
 #include <iostream>
 #include "mu-emu/core.h"
 #include "fmt/core.h"
 #include "mu-emu/tui.h"
+#include "argparse/argparse.hpp"
 
-int main() {
-    // auto vm = MuEmu::VM(1024 * 1024);
-    // vm.SetExecutionStrategy("RV32I");
-    // vm.PrintSomething("something");
-    // vm.Step();
+int main(int argc, char *argv[]) {
+    argparse::ArgumentParser parser("mu-emu");
+    parser.add_argument("--isa")
+        .help("Select the ISA. Currently supported ISAs include: RV32I")
+        .required();
+    parser.add_argument("--tui")
+        .help("Select the TUI. Currently supported ISAs include: Generic, RV32I")
+        .required();
 
-    MuEmu::TUI::start();
+    try {
+        parser.parse_args(argc, argv);
+    } catch (const std::runtime_error &err){
+        std::cerr << err.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+
+    auto tuiOption = parser.get<std::string>("--tui");
+    auto isaOption = parser.get<std::string>("--isa");
+    
+    auto vm = MuEmu::VM(1024 * 1024);
+    vm.SetExecutionStrategy(isaOption);
+    // TODO: Change the TUI interface to look like the VM
+    auto tui = MuEmu::ISA::RV32I::TUIStrategy(vm);
+    tui.start();
 }
